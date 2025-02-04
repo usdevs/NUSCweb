35f28836da41350d6561de14eb970453de52dcd5
shawnnygoh <shawnnygoh@gmail.com>
2025-02-05 00:39:57 +0800

    Update Metadata Title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'NUS College Club',
+  description: 'NUS College Student Life Portal',
+}
+
 export default function RootLayout({
   children,
 }: {
