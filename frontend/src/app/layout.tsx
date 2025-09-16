a84140fdef76dac86ffe22f7b3903274e80048fe
TheMythologist <leekaixuan2001@gmail.com>
2025-09-16 21:05:57 +0800

    Add Vercel analytics and speed insights

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
 
 import Footer from '@/components/Footer';
@@ -27,6 +29,8 @@ export default function RootLayout({
           <Toaster richColors position='bottom-center' />
           <Footer />
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
