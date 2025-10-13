b4caee1242112934528189f34a23155cd020b291
TheMythologist <leekaixuan2001@gmail.com>
2025-10-13 11:15:23 +0800

    chore: Disable Posthog during development

--- frontend/src/instrumentation-client.ts
+++ frontend/src/instrumentation-client.ts
@@ -3,4 +3,9 @@ import posthog from 'posthog-js';
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
+  loaded: function (ph) {
+    if (process.env.NODE_ENV === 'development') {
+      ph.opt_out_capturing();
+    }
+  },
 });
