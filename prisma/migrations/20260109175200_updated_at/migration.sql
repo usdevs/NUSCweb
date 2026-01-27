0016295d91a1363e38ec72b920133d6c8b16b42a
TheMythologist <leekaixuan2001@gmail.com>
2026-01-27 15:50:26 -0800

    Update migration script

--- prisma/migrations/20260109175200_updated_at/migration.sql
+++ prisma/migrations/20260109175200_updated_at/migration.sql
@@ -1,6 +1,6 @@
 -- AlterTable
-ALTER TABLE "public"."organisations" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "public"."users" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "public"."user_on_org" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "public"."bookings" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "public"."events" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "public"."organisations" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "public"."users" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "public"."user_on_org" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "public"."bookings" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "public"."events" ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
