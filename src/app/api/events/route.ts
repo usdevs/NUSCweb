87c98bf2ea19f84955d56ee81ab480f13c691417
pengkiang <pengkiangtan@gmail.com>
2026-03-26 22:03:28 +0800

    fix: use main version of booking and events routes

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -35,7 +35,7 @@ export async function GET(request: NextRequest) {
 
     if (venue) {
       events = events.filter(
-        (event) => event.booking?.venue?.name === venue
+        (event: { booking: { venue: { name: string; }; }; }) => event.booking?.venue?.name === venue
       );
     }
 
